2f6f1b96fed6e8c307643a3895c738d57da593d8
Rafael Ribeiro <work.rafaelribeiro@gmail.com>
2024-01-06 14:40:25 +0000

    GH-3 Add anime list to the library interface

--- mal-kotlin/src/commonMain/kotlin/com/github/rribeiro5/malkotlin/MALClient.kt
+++ mal-kotlin/src/commonMain/kotlin/com/github/rribeiro5/malkotlin/MALClient.kt
@@ -1,8 +1,13 @@
 package com.github.rribeiro5.malkotlin
 
 import com.github.rribeiro5.malkotlin.di.MALContainer
+import com.github.rribeiro5.malkotlin.model.anime.AnimeListNode
+import com.github.rribeiro5.malkotlin.model.core.PaginatedList
+import com.github.rribeiro5.malkotlin.service.AnimeService
 
 class MALClient internal constructor(container: MALContainer) {
+
+    private val animeService: AnimeService = container.animeService
 
     constructor(
         apiKey: String,
@@ -12,5 +17,17 @@ class MALClient internal constructor(container: MALContainer) {
             apiKey = apiKey,
             timeoutMillis = timeoutMillis
         )
+    )
+
+    suspend fun getAnimeList(
+        query: String?,
+        limit: Int?,
+        offset: Int?,
+        fields: String?
+    ): Result<PaginatedList<AnimeListNode>> = animeService.getAnimeList(
+        query = query,
+        limit = limit,
+        offset = offset,
+        fields = fields
     )
 }
